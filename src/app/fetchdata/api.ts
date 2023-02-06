13469bd9956e2a3839c8591c50ab32c84f741176
TheisTheGreat <theishove@gmail.com>
2023-02-06 21:06:30 +0100

    component

---
+++ src/app/fetchdata/api.ts
@@ -0,0 +1,20 @@
+export interface IPokemon {
+
+    abilities: any[];
+    forms: any[];
+    game_indices: any[];
+    height: number;
+    held_items: any[];
+    id: number;
+    is_default: boolean;
+    location_area_encounters: string;
+    moves: any[];
+    name: string;
+    order: number;
+    past_types: any[];
+    species: object;
+    sprites: object;
+    stats: any[];
+    types: any[];
+    weight: number;
+}
